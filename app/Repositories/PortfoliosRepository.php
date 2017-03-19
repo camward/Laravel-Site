a918e504dc213dee0914facf30c891a3e7a81cef
alex <camward@yandex.ru>
2017-03-19 18:11:56 +0400

    Портфолио. Детальная страница php artisan make:migration ChangePortfoliosTable2 --table=portfolios

--- app/Repositories/PortfoliosRepository.php
+++ app/Repositories/PortfoliosRepository.php
@@ -8,4 +8,11 @@ class PortfoliosRepository extends ARepository {
     public function __construct(Portfolio $portfolio) {
         $this->model = $portfolio;
     }
+    public function one($alias,$attr = array()) {
+        $portfolio = parent::one($alias,$attr);
+        if($portfolio && $portfolio->img) {
+            $portfolio->img = json_decode($portfolio->img);
+        }
+        return $portfolio;
+    }
 }
