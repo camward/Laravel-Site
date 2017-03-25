ed0652275cff5138618d6c1d640a4a2346478aed
alex <camward@yandex.ru>
2017-03-25 22:47:29 +0400

    Раздел администратора. Редактирование прав пользователя php artisan make:controller PermissionsController --resource

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,7 @@ Route::group(['prefix' => 'admin','middleware'=> 'auth'],function() {
     //admin
     Route::get('/',['uses' => 'Admin\IndexController@index','as' => 'adminIndex']);
     Route::resource('/articles','Admin\ArticlesController');
-
+    Route::resource('/permissions','Admin\PermissionsController');
 });
 
 Route::get('articles/cat/{cat_alias?}', ['uses'=>'ArticlesController@index','as'=>'articlesCat']);
