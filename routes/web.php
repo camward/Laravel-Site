bb75059a0f128d2072dccb848607fe00b215b9c4
alex <camward@yandex.ru>
2017-03-26 20:19:29 +0400

    Раздел администратора. Список пунктов меню php artisan make:controller MenusController --resource

--- routes/web.php
+++ routes/web.php
@@ -45,6 +45,7 @@ Route::group(['prefix' => 'admin','middleware'=> 'auth'],function() {
     Route::get('/',['uses' => 'Admin\IndexController@index','as' => 'adminIndex']);
     Route::resource('/articles','Admin\ArticlesController');
     Route::resource('/permissions','Admin\PermissionsController');
+    Route::resource('/menus','Admin\MenusController');
 });
 
 Route::get('articles/cat/{cat_alias?}', ['uses'=>'ArticlesController@index','as'=>'articlesCat']);
